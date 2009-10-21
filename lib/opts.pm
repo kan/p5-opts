9efb2471d5b4948b14f51e620a8fc9214d18b65e
Kan Fushihara <kan@kan-loox.(none)>
2009-10-22 00:28:31 +0900

    initial commit

---
+++ lib/opts.pm
@@ -0,0 +1,33 @@
+package opts;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+opts -
+
+=head1 SYNOPSIS
+
+  use opts;
+
+=head1 DESCRIPTION
+
+opts is
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan.fushihara at gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
